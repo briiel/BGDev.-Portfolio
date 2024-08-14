3d16444f4c6b426b8d2b389a8d5faa837ee15c32
briiel <144537019+briiel@users.noreply.github.com>
2024-08-15 00:53:08 +0800

    Added Footer and IconTop
    added footer and a functionality to go from the last section up to the home section.

--- Portfolio/assets/js/main.js
+++ Portfolio/assets/js/main.js
@@ -1,21 +1,52 @@
 /*Scroll Reveal*/
-ScrollReveal ({
-    distance: '80px',
-    duration: 600,
-    delay: 600,
+ScrollReveal({
+  distance: "80px",
+  duration: 600,
+  delay: 600,
 });
 
-ScrollReveal().reveal('.home-content, heading', { origin: 'top'});
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom'});
-ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left'});
-ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right'});
-
+ScrollReveal().reveal(".home-content, heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .services-container, .portfolio-box, .contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-contact h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-contact p, .about-content", { origin: "right" });
+ScrollReveal().reveal(".footer-iconTop", { origin: "bottom", delay: 100 });
 
 /*Typed Js*/
-const typed = new Typed ('.multiple-text',{
-    strings: ['Frontend Developer', 'Graphic Designer', 'Video Editor'],
-    typeSpeed: 70,
-    backSpeed: 70,
-    backDelay: 1000,
-    loop: true,
+const typed = new Typed(".multiple-text", {
+  strings: ["Frontend Developer", "Graphic Designer", "Video Editor"],
+  typeSpeed: 70,
+  backSpeed: 70,
+  backDelay: 1000,
+  loop: true,
+});
+
+/*footer-iconTop JS*/
+/*footer-iconTop JS*/
+document.addEventListener("DOMContentLoaded", () => {
+  const homeSection = document.getElementById("home");
+  const footerIconTop = document.querySelector(".footer-iconTop");
+
+  if (
+    homeSection.offsetTop <= window.scrollY &&
+    window.scrollY < homeSection.offsetTop + homeSection.offsetHeight
+  ) {
+    footerIconTop.style.opacity = 0;
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const homeSection = document.getElementById("home");
+  const footerIconTop = document.querySelector(".footer-iconTop");
+
+  if (
+    homeSection.offsetTop <= window.scrollY &&
+    window.scrollY < homeSection.offsetTop + homeSection.offsetHeight
+  ) {
+    footerIconTop.style.opacity = 0;
+  } else {
+    footerIconTop.style.opacity = 1;
+  }
 });
